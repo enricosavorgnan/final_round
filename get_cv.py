0672fb8eb869bb429de8689e4cc58034ab92411f
enricosavorgnan <115220846+enricosavorgnan@users.noreply.github.com>
2024-06-02 10:53:48 +0200

    Update get_cv.py

--- get_cv.py
+++ get_cv.py
@@ -1,18 +1,27 @@
 import cv2
 import torch
 
-# Load the model Yolov5
+# Upload the YOLOv5 model
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
 
-# Function to detect and count people
+
 def detect_and_count_people(image):
+    # ------------------------------------------------------------------- #
+    # This function is used to detect and count the number of people in   #
+    # the image.                                                          #
+    # input: image - the image to be processed                            #
+    # output: image - the image with the detections                       #
+    #         len(people_detections) - the number of people detected       #
+    # ------------------------------------------------------------------- #
+
+    # Detect objects in the image
     results = model(image)
     detections = results.pandas().xyxy[0]
 
-    # Filtering people 
+    # >Filters people
     people_detections = detections[detections['name'] == 'person']
 
-    # Drawing bounding boxes and counting people
+    # Draw bounding boxes and count people
     for _, detection in people_detections.iterrows():
         xmin, ymin, xmax, ymax = int(detection['xmin']), int(detection['ymin']), int(detection['xmax']), int(detection['ymax'])
         cv2.rectangle(image, (xmin, ymin), (xmax, ymax), (255, 0, 0), 2)
@@ -22,20 +31,24 @@ def detect_and_count_people(image):
 
 
 def main():
+    # ------------------------------------------------------------------- #
+    # This function is used to get the number of people in the image.     #
+    # output: people_count - the number of people in the image            #
+    # ------------------------------------------------------------------- #
+
     # Load the image
-    image_path = 'img/room.jpg'
+    image_path = 'media/room.jpg'
     image = cv2.imread(image_path)
 
-    # Detects and counts people in the image
+    # Detect and count people in the image
     image, people_count = detect_and_count_people(image)
 
-    # Shows the image with detection
+    # Show the image with the detections
     cv2.putText(image, f'People Count: {people_count}', (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     cv2.imshow('YOLOv5 People Detection', image)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-    Saves image with detections 
-    cv2.imwrite('img/room2.jpg', image)
+    cv2.imwrite('media/room_detected.jpg', image)
 
     return people_count
